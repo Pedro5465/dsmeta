e5a6ef6b54af019b9cb5a2e3735564945d204bd0
Pedro <108467823+Pedro5465@users.noreply.github.com>
2022-07-17 08:47:55 -0400

    SMS

--- backend/src/main/java/com/projeto/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/projeto/dsmeta/controllers/SaleController.java
@@ -1,12 +1,10 @@
 package com.projeto.dsmeta.controllers;
 import com.projeto.dsmeta.entities.Sale;
 import com.projeto.dsmeta.services.SaleService;
+import com.projeto.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
 
@@ -15,11 +13,19 @@ import org.springframework.data.domain.Pageable;
 public class SaleController {
     @Autowired
     private SaleService service;
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSale(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id ) {
+        smsService.sendSms(id);
     }
 }
